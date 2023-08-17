7ba8dbc2be7e77c664e1c1c848035ac69ae3c3c1
MrAfitol <76150070+MrAfitol@users.noreply.github.com>
2023-08-17 14:47:03 +0300

    1.0.1
    Added new events
    Added mod description
    Added framework package

--- TemplateMod/EventHandlers.cs
+++ TemplateMod/EventHandlers.cs
@@ -5,6 +5,16 @@ namespace TemplateMod
 {
     public class EventHandlers
     {
+        public void OnSetItemToInfinite(SetItemToInfiniteCraftEventArgs ev)
+        {
+            Logger.Debug($"You set {ev.Item.itemName} to infinite craft");
+        }
+
+        public void OnSetItemToCraft(SetItemToCraftEventArgs ev)
+        {
+            Logger.Debug($"You set {ev.Item.itemName} to craft of quantity {ev.Quantity}");
+        }
+
         public void OnPickupedItem(PickupedItemEventArgs ev)
         {
             Logger.Debug($"You pick up a {ev.ItemInfo.itemName} of quantity {ev.Quantity}");
@@ -18,6 +28,11 @@ namespace TemplateMod
         public void OnGameLoaded()
         {
             Logger.Debug("Game loaded!");
+        }
+
+        public void OnMenuLoaded()
+        {
+            Logger.Debug("Menu loaded!");
         }
     }
 }
--- TemplateMod/Mod.cs
+++ TemplateMod/Mod.cs
@@ -11,7 +11,8 @@ namespace TemplateMod
     {
         public override string Author { get; set; } = "MrAfitol";
         public override string Name { get; set; } = "Template Mod";
-        public override Version Version { get; set; } = new Version(1, 0, 0);
+        public override string Description { get; set; } = "A mod template for the Outpath-Modding framework";
+        public override Version Version { get; set; } = new Version(1, 0, 1);
 
         public static Mod Instance { get; private set; }
 
@@ -23,9 +24,12 @@ namespace TemplateMod
             Instance = this;
             eventsHandler = new EventHandlers();
 
+            EventsManager.SetItemToInfiniteCraft += eventsHandler.OnSetItemToInfinite;
+            EventsManager.SetItemToCraft += eventsHandler.OnSetItemToCraft;
             EventsManager.PickupedItem += eventsHandler.OnPickupedItem;
             EventsManager.TakeOutResource += eventsHandler.OnTakeOutResource;
             EventsManager.GameLoaded += eventsHandler.OnGameLoaded;
+            EventsManager.MenuLoaded += eventsHandler.OnMenuLoaded;
 
             CommandManager.AddCommand(new Test());
 
